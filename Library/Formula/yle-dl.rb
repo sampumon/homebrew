702cfa30fa37067f971b8a8586f376ee45e55bab
sampumon <samuli.kaipiainen@helsinki.fi>
2012-06-21 16:30:43 +0300

    yle-dl 2.0.0

--- Library/Formula/yle-dl.rb
+++ Library/Formula/yle-dl.rb
@@ -1,21 +1,37 @@
 require 'formula'
 
-class YleDl < Formula
-  # TODO: use git repository and depend on rtmpdump. would it work?
-  homepage 'http://users.tkk.fi/~aajanki/rtmpdump-yle/'
-  url 'http://users.tkk.fi/~aajanki/rtmpdump-yle/yle-dl-1.99.7.tar.gz'
-  md5 '96b6bbe5b37e71539555f48426f8b672'
+class PyCrypto < Requirement
+  def message; <<-EOS.undent
+      Detected unsatisfied Python dependency. Install with:
+        easy_install pycrypto
+    EOS
+  end
+  def satisfied?
+    quiet_system "/usr/bin/env python -c 'from Crypto.Cipher import AES'"
+  end
+end
 
-  # TODO: check dependencies
+class YleDl < Formula
+  # TODO: head at github
+  homepage 'http://users.tkk.fi/~aajanki/rtmpdump-yle/'
+  url 'http://users.tkk.fi/~aajanki/rtmpdump-yle/yle-dl-2.0.0.tar.gz'
+  md5 '08d84d654100da2da7c1a72dacc652b4'
+
+  head 'git://github.com/aajanki/yle-dl.git'
+
+  # TODO: depends on json?
+  depends_on 'rtmpdump'
+  depends_on PyCrypto.new
+  # this dependancy check fails: depends_on 'pycrypto' => :python
 
   def install
-    system "make SYS=darwin"
-    system "mkdir -p #{lib}" # hack for rtmpdump/librtmp/Makefile#120
+    # TODO: why is this not automatic?
+    # system "easy_install pycrypto"
     system "make install SYS=darwin prefix=#{prefix} mandir=#{man}"
   end
 
   def test
-    # TODO: remove downloaded video
+    # TODO: remove downloaded video?
     ohai "Getting the latest news for you."
     system "yle-dl --latestepisode --resume -o uutiset.flv http://areena-beta.yle.fi/ng/areena?q=uutiset"
 
@@ -27,8 +43,9 @@ class YleDl < Formula
   end
 
   def caveats; <<-EOS.undent
-    We're going to install a modified version of rtmpdump.
-    If you already installed rtmpdump via brew, this probably fails.
+      Requires PyCrypto Python library. https://www.dlitz.net/software/pycrypto/
+      Can be installed with:
+        easy_install pycrypto
     EOS
   end
 end
